159ff6f4388f336cd7d44a0f23ccd96fd43b480a
sambegdhakal <sambegdhakal@gmail.com>
2024-04-11 16:29:45 -0500

    Adding changes to sambeg-branch

--- registration/models.py
+++ registration/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.hashers import make_password
 
 class PatreonUser(AbstractUser):
+    userid = models.AutoField(primary_key=True)
     username = models.CharField(max_length=55, unique=True)
     password = models.CharField(max_length=128)  # Increase max_length to accommodate hashed passwords
     
@@ -10,3 +11,7 @@ class PatreonUser(AbstractUser):
         # Hashed the password before saving
         self.password = make_password(self.password)
         super().save(*args, **kwargs)
+
+# Specify unique related_name arguments for the conflicting fields
+PatreonUser._meta.get_field('groups').remote_field.related_name = 'patreon_user_groups'
+PatreonUser._meta.get_field('user_permissions').remote_field.related_name = 'patreon_user_permissions'
