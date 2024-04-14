408428f9530a888279d06e890f625594921d2f41
anupam <anupam.siwakoti@gmail.com>
2024-04-14 15:36:39 -0500

    checking the changes in my branch

--- page/views.py
+++ page/views.py
@@ -2,7 +2,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import PatreonPage
 from registration.models import PatreonUser
-from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.http import JsonResponse
 from django.core.serializers import serialize
@@ -12,7 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 def patreon_page_list(request):
     user = PatreonUser.objects.get(username = request.session["user"])
     pages = PatreonPage.objects.filter(creator=user)
-    data = [{'id': page.id, 'description': page.description, 'goal amount': page.goal_amount, 'creator': page.creator.username} for page in pages]
+    data = [{'id': page.id, 'description': page.description, 'goal amount': page.goal_amount, 'creator': page.creator.username, 'current amount': page.current_amount} for page in pages]
     return JsonResponse(data, safe=False)
 
 # @login_required
@@ -71,9 +70,8 @@ def patreon_page_donation(request, id):
         # i need the curent logged in user, to save to donated by
         page = PatreonPage.objects.get(id=id)
         donation_amount = request.POST.get('donation_amt')
-        page.donation = donation_amount
+        page.current_amount = page.current_amount + int(donation_amount)
         page.save()
         response = JsonResponse({'pageid':id ,'msg':"donation updated"})
-        
+        response["Access-Control-Allow-Origin"] = "http://localhost:3000"
         return response
-    return render(request, 'patreon/page_confirm_delete.html', {'page': page})
