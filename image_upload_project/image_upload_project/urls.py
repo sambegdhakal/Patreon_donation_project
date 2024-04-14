5c7a2920cd4d02bc15fff61df5121b57777bf7b4
sambegdhakal <sambegdhakal@gmail.com>
2024-04-14 17:25:28 -0500

    New upload image and retreive codes

---
+++ image_upload_project/image_upload_project/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for image_upload_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from upload_profile_image.views import upload_image
+from upload_profile_image.views2 import get_images_by_user
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('upload/', upload_image, name='upload_image'),
+    path('images/<int:user_id>/', get_images_by_user, name='get_images_by_user'),    
+]
