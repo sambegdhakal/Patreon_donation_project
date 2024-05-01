55257524b4b45aaf830f42d74d5746119bf2290d
sambegdhakal <36032218+sambegdhakal@users.noreply.github.com>
2024-04-30 19:09:01 -0500

    Update urls.py
    removed unnecessary url

--- page/urls.py
+++ page/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path("viewpage/<int:id>/", views.patreon_page_list, name= "patreon_page_list"),
     path("deletepage/<int:id>/", views.patreon_page_delete, name= "patreon_page_delete"),
     path("updatepage/<int:id>/", views.patreon_page_update, name= "patreon_page_update"),
-    path("pagedonation/<int:pageid>/<int:userid>/", views.patreon_page_donation, name= "patreon_page_donation"),
     path("subscribe/<int:pageid>/<int:userid>/", views.subscribe_to_page, name= "patreon_page_subscribe"),
 ]
